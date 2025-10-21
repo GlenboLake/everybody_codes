19102361dc4295818b5fca576684775019931023
Glen Bradley <glenbolake@gmail.com>
2025-10-21 19:35:49 -0400

    Quest 4

---
+++ kingdom_of_algorithmia/quest04.py
@@ -0,0 +1,22 @@
+from statistics import median
+from typing import Collection
+
+
+def parse(filename):
+    with open(filename) as f:
+        return [int(line) for line in f]
+
+
+def part1_2(nums: Collection[int]):
+    return sum(nums) - min(nums) * len(nums)
+
+
+def part3(nums: Collection[int]):
+    target = int(median(nums))
+    return sum(abs(n-target) for n in nums)
+
+
+if __name__ == '__main__':
+    print(part1_2(parse('everybody_codes_e2024_q04_p1.txt')))
+    print(part1_2(parse('everybody_codes_e2024_q04_p2.txt')))
+    print(part3(parse('everybody_codes_e2024_q04_p3.txt')))
---
+++ kingdom_of_algorithmia/tests/test_quest04.py
@@ -0,0 +1,9 @@
+from kingdom_of_algorithmia import quest04
+
+
+def test_part1():
+    assert quest04.part1_2([3, 4, 7, 8]) == 10
+
+
+def test_part3():
+    assert quest04.part3([2, 4, 5, 6, 8]) == 8
